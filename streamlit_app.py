10a28de3fe08d51aa5778319391a17dd958ff6fb
yblebon <135013954+yblebon@users.noreply.github.com>
2023-09-07 19:34:22 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,12 @@ QUALITE DES EAUX DE BAIGNADE
 """)
 
 plages = {
-    'SAINT LEU: Plage citerne quarante-six 2023 prelevement': 1,
+    'SAINT LEU: Plage citerne quarante-six 2023 prelevement': {
+        'id': 1,
+        'img': 'IMG_2367.jpeg',
+        
+    },
+    
     'SAINT GILLES: Plage ermitage 2022 prelevement': 2
 }
 
@@ -42,65 +47,6 @@ image = Image.open(image_name)
 st.image(image, caption='Source')
 
           
-data = {
- 'date': [
-  "03/01/23", 
-  "10/01/23",
-  "17/01/23",
-  "24/01/23",
-  "31/01/23",
-  "07/02/23", 
-  "14/02/23",
-  "21/02/23",
-  "28/02/23",
-  "07/03/23",
-  "14/03/23", 
-  "21/03/23", 
-  "28/03/23", 
-  "04/04/23", 
-  "13/04/23", 
-  "18/04/23", 
-  "25/04/23", 
-  "02/05/23", 
-  "09/05/23", 
-  "23/05/23",
-  "06/06/23", 
-  "04/07/23", 
-  "18/07/23", 
-  "25/07/23", 
-  "01/08/23", 
-  "08/08/23"
- ],
- 'escherichia coli':
- [
-  0,
-  0,
-  94,
-  0,
-  0,
-  0,
-  0,
-  15,
-  0,
-  0,
-  0,
-  0,
-  46,
-  15,
-  0,
-  61,
-  0,
-  0,
-  0,
-  0,
-  0,
-  15,
-  0,
-  0,
-  0,
-  0
- ]
-}
 
 df = pd.DataFrame.from_dict(data)
 df['date'] = pd.to_datetime(df['date'], format="%d/%m/%y")
