ece1cfb35a753da6fb5c84ebe1383e97f95cc014
yblebon <135013954+yblebon@users.noreply.github.com>
2023-09-07 16:59:09 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,17 @@ import math
 import random
 from PIL import Image
 
-option = st.selectbox(
-    'Choose the beach?',
-    ('SAINT LEU: Plage citerne quarante-six 2023 prelevement', 'SAINT GILLES: Plage ermitage 2022 prelevement'))
-
-st.write('You selected:', option)
 
 
 st.title("""
 QUALITE DES EAUX DE BAIGNADE
-SAINT LEU: Plage citerne quarante-six 2023 prelevement 
 """)
+
+option = st.selectbox(
+    'Choose the beach?',
+    ('SAINT LEU: Plage citerne quarante-six 2023 prelevement', 'SAINT GILLES: Plage ermitage 2022 prelevement'))
+
+st.write('Plage:', option)
 
 st.header("""
 Source de donnees
